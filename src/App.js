d826f4fe1eabe0d46e05474b8a966740ae2ebfb4
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-01-21 00:18:34 -0300

    fetch data. parse data with papaparse

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Papa from "papaparse";
 
 const App = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        'https://docs.google.com/spreadsheets/d/e/2PACX-1vTdffsUIOaXwUOPy17qE8Hz1TZNVofcWmiv0e3-04Knv0t3xPyoCl5iJQOlauM1qYAbd6X45-ql71s5/pub?output=csv',
+        {
+          responseType: 'blob'
+        }
+      ).then(
+        (response) => {
+          new Promise ((resolve, reject) => {
+            Papa.parse(response.data, {
+              download: true,
+              header: true,
+              complete: results => {
+                const productsList = results.data;
+                return resolve(setProducts(productsList))
+              }
+            })
+          })
+        }
+      )
+    }
+    fetchData();
+  }, []);
+
+  console.log("products", products)
+
+  
   return (
     <div className="App">
       <h1>AppMenu</h1>
