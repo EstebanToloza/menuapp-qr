8dccfe2fc7408bbafa157bd1f285fc185ea26cb8
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-03-15 22:45:06 -0300

    cart resume, calc total. add/remove item from resume

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,49 +1,14 @@
 import React, { useContext, useState } from 'react';
 import { BagCheck } from 'react-bootstrap-icons';
 import { CartContext } from '../context/CartContext';
-import Modal from 'react-bootstrap/Modal';
 import { Button } from 'react-bootstrap';
+import CartResume from './CartResume';
 
 const Cart = () => {
 
     const { cartItems } = useContext(CartContext);
-    console.log(cartItems)
-
     const [showCart, setShowCart] = useState(false);
-    const handleCloseCart = () => setShowCart(false);
-    //const handleShowCart = () => setShowCart(true);
 
-    const CartResume = () => {
-
-        return (
-            <Modal show={showCart} onHide={handleCloseCart} centered>
-            <Modal.Header closeButton>
-              <Modal.Title>Orden Mesa 2</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {cartItems.map(item => {
-                    const itemTotal = item.amount * parseInt(item.price);
-
-                    return (
-                        <div key={item.id} className="cart-container-product mb-3 py-3">
-                            <div className='cart-product-info'>
-                                <span className='item-name mr-2'>{item.dish}</span>
-                                x
-                                <span className='item-amount ml-2'>{item.amount}u.</span>
-                            </div>
-                            <div className='cart-product-price-and-actions'>
-                                <span className='item-total'>{`$${itemTotal}`}</span>
-                                <div className='item-actions'>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-
-            </Modal.Body>
-          </Modal>
-        )
-    }
 
     return (
         <>
@@ -53,7 +18,7 @@ const Cart = () => {
                     <span>{cartItems.length}</span>
                 </div>
             </div>
-            {showCart && <CartResume />}
+            {showCart && <CartResume cartItems={cartItems} showCart={showCart} setShowCart={setShowCart}/>}
         </>
     )
 }
---
+++ src/components/CartResume.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { CartContext } from '../context/CartContext';
+
+
+const CartResume = ({cartItems, showCart, setShowCart}) => {
+    const { DeleteItemToCart, AddItemToCart } = useContext(CartContext);
+    const handleCloseCart = () => setShowCart(false);
+    const cartTotal = cartItems.reduce((prev, current) => prev + current.amount * current.price, 0);
+
+  return (
+    <Modal show={showCart} onHide={handleCloseCart} centered>
+    <Modal.Header closeButton>
+      <Modal.Title>Orden Mesa 2</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        {cartItems.map(item => {
+            const itemTotal = item.amount * parseInt(item.price);
+            return (
+                <div key={item.id} className="cart-container-product mb-3 py-3">
+                    <div className='cart-product-info d-flex flex-column'>
+                        <span className='item-name'>{item.dish}</span>
+                        <span className='item-amount'>x {item.amount}u.</span>
+                    </div>
+                    <div className='cart-product-price-and-actions'>
+                        <span className='item-total'>{`$${itemTotal}`}</span>
+                        <div className='item-actions'>
+                        <div className="add-remove-cart-container">
+                            <button 
+                            onClick={() => DeleteItemToCart(item.id)}
+                            //disabled={inCart ? false : true}
+                            >-</button>
+                            <span>{item.amount}</span>
+                            <button onClick={() => AddItemToCart(item)}>+</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
+        <div className='cart-total-container border-top pt-3 d-flex justify-content-between'>
+            <span className='total-text'>Total a pagar:</span>
+            <span className='total-price'>{`$${cartTotal}`}</span>
+        </div>
+
+    </Modal.Body>
+  </Modal>
+  )
+}
+
+export default CartResume
